3fc8461e7ec7f9d64a79614a089f09c7a8b69a1e
Leonardo Torres <leojts24@gmail.com>
2024-12-10 14:23:30 -0600

    finish screens login and register

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -12,13 +12,28 @@ import {EvaIconsPack} from '@ui-kitten/eva-icons';
 export default function ProductsApp() {
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? eva.dark : eva.light;
+  const backgroundColor =
+    colorScheme === 'dark'
+      ? theme['color-basic-800']
+      : theme['color-basic-100'];
 
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
 
       <ApplicationProvider {...eva} theme={theme}>
-        <NavigationContainer>
+        <NavigationContainer
+          theme={{
+            dark: colorScheme === 'dark',
+            colors: {
+              primary: theme['color-primary-500'],
+              background: backgroundColor,
+              card: theme['color-basic-100'],
+              text: theme['color-basic-color'],
+              border: theme['color-basic-800'],
+              notification: theme['color-primary-500'],
+            },
+          }}>
           <StackNavigator />
         </NavigationContainer>
       </ApplicationProvider>
